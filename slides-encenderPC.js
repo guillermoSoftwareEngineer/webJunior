039f3bb29c9fe90e88f4dfc5e23776ede139e8ab
guillermoSoftwareEngineer <guillermovasbendev@gmail.com>
2025-07-27 18:27:05 -0500

    actualizacion slides al encender PC

--- slides-encenderPC.js
+++ slides-encenderPC.js
@@ -5,14 +5,17 @@ menuToggle.addEventListener('click', () => {
 });
 
 const slides = [
-  './imageSlides/slides-analisis-1.png',
-  './imageSlides/slides-analisis-2.png',
-  './imageSlides/slides-analisis-3.png',
-  './imageSlides/slides-analisis-4.png',
-  './imageSlides/slides-analisis-5.png',
-  './imageSlides/slides-analisis-6.png',
-  './imageSlides/slides-analisis-7.png',
-  './imageSlides/slides-analisis-8.png'
+  './imageSlides/slides-encenderPC-1.jpg',
+  './imageSlides/slides-encenderPC-2.jpg',
+  './imageSlides/slides-encenderPC-3.jpg',
+  './imageSlides/slides-encenderPC-4.jpg',
+  './imageSlides/slides-encenderPC-5.jpg',
+  './imageSlides/slides-encenderPC-6.jpg',
+  './imageSlides/slides-encenderPC-7.jpg',
+  './imageSlides/slides-encenderPC-8.jpg',
+  './imageSlides/slides-encenderPC-9.jpg',
+  './imageSlides/slides-encenderPC-10.jpg',
+  './imageSlides/slides-encenderPC-11.jpg'
 ];
 
 let currentIndex = 0;
